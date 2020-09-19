a3083d7d19db25702c5deba6815e324defda0144
KangHyeonSeok <31717201+KangHyeonSeok@users.noreply.github.com>
2020-09-19 22:54:22 +0900

    카카오톡 기본 응답 추가.

---
+++ app/src/main/java/com/whyk/chatrpg/KakaotalkListener.kt
@@ -0,0 +1,86 @@
+package com.whyk.chatrpg
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.RemoteInput
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+
+@SuppressLint("OverrideAbstract")
+class KakaotalkListener() : NotificationListenerService() {
+    companion object {
+        var execContext: Context? = null
+    }
+
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+
+        Log.d("ChatRPG","onNoti")
+
+        if (sbn!!.packageName == "com.kakao.talk") {
+            val wExt = Notification.WearableExtender(
+                sbn.notification
+            )
+            for (act in wExt.actions) if (act.remoteInputs != null && act.remoteInputs.size > 0) if (act.title.toString()
+                    .toLowerCase().contains("reply") ||
+                act.title.toString().toLowerCase().contains("Reply") ||
+                act.title.toString().toLowerCase().contains("답장")
+            ) {
+                execContext = applicationContext
+                callResponder(
+                    sbn.notification.extras.getString(Notification.EXTRA_SUMMARY_TEXT),
+                    sbn.notification.extras.getString(
+                        Notification.EXTRA_TITLE
+                    ),
+                    sbn.notification.extras["android.text"],
+                    act
+                )
+            }
+        }
+    }
+
+    private fun callResponder(
+        room: String?,
+        sender: String?,
+        msg: Any?,
+        session: Notification.Action
+    ) {
+        var room: String? = room
+        val isGroupChat: Boolean
+        var _msg: String
+        if (room == null) {
+            room = sender
+            isGroupChat = false
+        } else {
+            isGroupChat = true
+        }
+
+        Log.d("ChatRPG", room + " / " + isGroupChat + " / " + msg)
+        reply("(꽃)", session)
+    }
+
+    fun reply(value: String?, session: Notification.Action) {
+        if (session == null) return
+        val sendIntent = Intent()
+        val msg = Bundle()
+
+        for (inputable in session.getRemoteInputs()) {
+            msg.putCharSequence(inputable.resultKey, value)
+            Log.d("ChatRPG", inputable.resultKey)
+        }
+
+        RemoteInput.addResultsToIntent(session.getRemoteInputs(), sendIntent, msg)
+
+        try {
+            session.actionIntent.send(execContext, 0, sendIntent)
+        } catch (e: PendingIntent.CanceledException) {
+        }
+    }
+}
--- app/src/main/java/com/whyk/chatrpg/MainActivity.kt
+++ app/src/main/java/com/whyk/chatrpg/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.whyk.chatrpg
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var listener = object : PermissionListener {
+            override fun onPermissionGranted() {
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String?>?) {
+
+            }
+        }
+
+        TedPermission.with(this).setPermissionListener(listener)
+                .setRationaleMessage("파일 읽고 쓰기 위한 권한")
+                .setDeniedMessage("거부시 파일 작업시 오류")
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check()
+
+
+        setting.setOnClickListener {
+            startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"))
+        }
     }
 }
